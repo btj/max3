6b498c0a8d38f89eeda9954487f544ace916bfbd
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-02-18 18:01:43 +0100

    Initial commit

---
+++ max3/src/max3/Max3Test.java
@@ -0,0 +1,16 @@
+package max3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Max3Test {
+
+	@Test
+	void test() {
+		assert Max3.max3(10, 20, 30) == 30;
+		assert Max3.max3(30, 20, 10) == 30;
+		assert Max3.max3(20, 30, 10) == 30;
+	}
+
+}
